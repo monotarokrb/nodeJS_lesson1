7d4a3b3bdd1654b905c174eff405eb77d1abefe2
monotarokrb <anhlankrb@gmail.com>
2022-11-12 11:41:48 +0700

    use npm

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require ('http');
+
+const server = http.createServer((req, res) => {
+    console.log('run request...')
+    res.setHeader('Content-type','text/html');
+    res.write('<h3>Hello World!</h3>');
+    res.write('<h2>From Sarah with love</h2>');
+    res.end();
+    
+})
+
+server.listen(3000,'localhost',() => {
+    console.log('node.js server is running on port: 300');
+})
