fa8196d6f55168983493f9236260dd631d6abae7
monotarokrb <anhlankrb@gmail.com>
2022-11-12 12:06:41 +0700

    use npm

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-type','text/html');
     res.write('<h3>Hello World!</h3>');
     res.write('<h2>From Sarah with love</h2>');
+    res.write('<h2>save in new branch</h2>');
     res.end();
     
 })
